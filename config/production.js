a5cd3daa241318ef374f377227c6535bf7e46a3e
Lee Ingram <lee@soylentcola.com>
2020-04-07 16:42:02 -0700

    Adding production config

---
+++ config/production.js
@@ -0,0 +1,47 @@
+module.exports = {
+  'node' : {
+    'mode'         : 'production',
+// do you want your webclient to allow users to connect to any game => true
+// do you want your webclient to connect users only to your game => false
+    'connectAnywhere': true,
+
+// if you set this to port 80, you must run the server as root
+    'port'         : 443,
+
+// specific ip is optional (if your server has more than one)
+//    'ip'           : '208.52.189.89',
+
+    'socketUrl'    : 'http://localhost:5555',
+    'socketUrlSSL' : '',
+    'poweredBy'    : 'dome-client.js',
+    'session'      : {
+        'secret' : 'secret',
+        'key'    : 'express.sid'
+    }
+  },
+  
+// ssl is optional
+//  'ssl' : {
+//    'port' : 443,
+//    'key'  : 'config/ssl/BlahBlah.key',
+//    'cert' : 'config/ssl/BlahBlah.crt',
+//    'ca'   : 'config/ssl/intermediate.crt'
+//  },
+
+  // where it connects to
+  'moo' : {
+    'name' : 'EscapeFromTextov',
+    'host' : '54.226.170.35',
+    'port' : 6666
+  },
+
+  // specialized autocomplete for each player class
+  'autocomplete' : {
+    'p' : 'config/ac/player.txt'
+  },
+  'version' : {
+    'major' : '1',
+    'minor' : '2'
+    // build is pulled from git hash
+  }
+};
